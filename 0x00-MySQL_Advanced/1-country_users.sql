cc3ceffa6e045cef16ffc2bea4a266305d24b518
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-14 16:07:59 +0000

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- The script creates a table users with, id, email, name and country
+-- id is integer(never null), email string(never null and unique), name string(255 characters), country(enumeration and nevr null)
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
