7396af1398ba9629075b0cc7bf4512e80b3faffe
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-14 16:50:19 +0000

    update

--- 0x00-MySQL_Advanced/2-fans.sql
+++
@@ -1,6 +0,0 @@
--- Script that ranks country originals of brands, ordered by the number of fans
-
-SELECT origin, SUM(nb_fans) AS total_fans
-    FROM metal_bands
-    GROUP BY origin
-    ORDER BY total_fans DESC;
