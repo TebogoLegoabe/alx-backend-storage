de82945cc9542278c506df6c323a1ddf2dd9616a
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-15 15:39:07 +0000

    best band ever

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- SQL script that ranks country origins of bands, ordered by number of fans.
+-- column names(origin and nb_fans)
+
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
